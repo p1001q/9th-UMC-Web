c3ca8ae0f15e18b88098701d734a9855163fbbe6
p1001q <p1001q@gmail.com>
2025-10-05 17:19:04 +0900

    [Week03] 미션1 - 완료

--- Week03/src/App.tsx
+++ Week03/src/App.tsx
@@ -1,70 +1,10 @@
 import './App.css';
-import { Link, Route, Routes } from './router';
-import { useCurrentPath } from './router/useCurrentPath';
-
-const MatthewPage = () => {
-  const path = useCurrentPath();
-  return (
-    <div>
-      <h1>매튜 페이지</h1>
-      <p>현재 경로: {path}</p>
-    </div>
-  );
-};
-
-const AeongPage = () => {
-  const path = useCurrentPath();
-  return (
-    <div>
-      <h1>애옹 페이지</h1>
-      <p>현재 경로: {path}</p>
-    </div>
-  );
-};
-
-const JoyPage = () => {
-  const path = useCurrentPath();
-  return (
-    <div>
-      <h1>조이 페이지</h1>
-      <p>현재 경로: {path}</p>
-    </div>
-  );
-};
-
-const NotFoundPage = () => {
-  const path = useCurrentPath();
-  return (
-    <div>
-      <h1>404 - 페이지를 찾을 수 없습니다</h1>
-      <p>현재 경로: {path}</p>
-    </div>
-  );
-};
-
-export { MatthewPage, AeongPage, JoyPage, NotFoundPage };
-
-const Header = () => {
-  return (
-    <nav style={{ display: 'flex', gap: '10px' }}>
-      <Link to='/matthew'>MATTHEW</Link>
-      <Link to='/aeong'>AEONG</Link>
-      <Link to='/joy'>JOY</Link>
-      <Link to='/not-found'>NOT FOUND</Link>
-    </nav>
-  );
-};
+import MoviesPage from './pages/MoviesPage';
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path='/matthew' component={MatthewPage} />
-        <Route path='/aeong' component={AeongPage} />
-        <Route path='/joy' component={JoyPage} />
-        <Route path='/not-found' component={NotFoundPage} />
-      </Routes>
+    <MoviesPage />
     </>
   );
 }
---
+++ Week03/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+//아니 난 왜 이 파일이 기본 생성 안됐지? 내가 따로 만들어줬음
+//무튼 토큰같은 거 환경변수 파일에다 넣어서 관리하고 깃허브에 푸시하면 안됨.
+interface ImportMetaEnv {
+  readonly VITE_TMDB_API_KEY: string;
+  readonly VITE_TMDB_TOKEN: string;
+  // 필요하면 더 추가
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
