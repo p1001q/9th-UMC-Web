190902c15e5caddb6eace8bbbb57a0b056e0f888
p1001q <p1001q@gmail.com>
2025-11-11 03:59:36 +0900

    Refactor: ProtectedLayout 수정

--- Week06/src/layouts/HomeLayout.tsx
+++ Week06/src/layouts/HomeLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
     <div className="h-dvh flex flex-col">
-      <nav>네비게이션 바 입니다.</nav>
+      <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
